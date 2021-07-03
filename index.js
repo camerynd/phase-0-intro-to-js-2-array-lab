c039f042475649e0c28607aaace4647e760313d5
github username <github email address>
2021-07-02 18:04:58 -0600

    commit tree 151ca93f99f90f2a1f4ea9c4375aa88823e233e4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+
+}
